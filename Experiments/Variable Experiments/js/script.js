c90a943869e8a4a95835bf018acde2d0af5ce53d
Foti/Atomic <fotibuddy@gmail.com>
2023-09-12 15:19:38 -0400

    Learned about declaring variables!

--- Experiments/Variable Experiments/js/script.js
+++ Experiments/Variable Experiments/js/script.js
@@ -8,6 +8,12 @@
 
 "use strict";
 
+let backgroundShade = 0;
+let circleX = 250;
+let circleY = 250;
+let circleSize = 100;
+// "let" declares a variable
+
 /**
  * Description of preload
 */
@@ -20,16 +26,19 @@ function preload() {
  * Description of setup
 */
 function setup() {
-    createCanvas(windowWidth, windowHeight); // windowWidth and windowHeight makes the canvas the size of the whole webpage
-    
+    //createCanvas(windowWidth, windowHeight); // windowWidth and windowHeight makes the canvas the size of the whole webpage
+    createCanvas(500, 500);
 }
 
 
 /**
- * Description of draw() // can also be called an update function
+ * Description of draw() // can also be called an update function - executed 60 times per second
 */
 function draw() {
-    background(255, 0, 0); // can also be an image
-    rectMode(CENTER);
-    rect(mouseX, mouseY, 100, 100); // mouseX and mouseY changes every time as you move your mouse
+    //background(255, 0, 0); // can also be an image
+    //rectMode(CENTER);
+    //rect(mouseX, mouseY, 100, 100); // mouseX and mouseY changes every time as you move your mouse
+    
+    background(backgroundShade);
+    ellipse(circleX, circleY, circleSize);
 }
