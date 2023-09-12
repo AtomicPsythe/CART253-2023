71a395ad09b49dd91b2dc40f97674ffcd36a02a1
Foti/Atomic <fotibuddy@gmail.com>
2023-09-12 14:58:26 -0400

    Learned about using built-in variables!

--- Experiments/Variable Experiments/js/script.js
+++ Experiments/Variable Experiments/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Variable Experiments
+ * Foti Aivaliklis
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -20,13 +20,16 @@ function preload() {
  * Description of setup
 */
 function setup() {
-
+    createCanvas(windowWidth, windowHeight); // windowWidth and windowHeight makes the canvas the size of the whole webpage
+    
 }
 
 
 /**
- * Description of draw()
+ * Description of draw() // can also be called an update function
 */
 function draw() {
-
+    background(255, 0, 0); // can also be an image
+    rectMode(CENTER);
+    rect(mouseX, mouseY, 100, 100); // mouseX and mouseY changes every time as you move your mouse
 }
