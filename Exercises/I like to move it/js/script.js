ee2c17df4dde6478044881adc13b454dda3dc319
Foti Aivaliklis <fotibuddy@gmail.com>
2023-09-14 17:43:54 -0400

    E1: Completed most of the activity, but requires revisions

--- Exercises/I like to move it/js/script.js
+++ Exercises/I like to move it/js/script.js
@@ -16,68 +16,106 @@ let bg = {
 }
 let circle1 = {
     x: 0,
-    y: 250, 
-    size: 100,
-    speed: 1,
-    fill: 255
+    y: 66, 
+    size: 130,
+    speed: 2, 
+    fill: 200
+};
+let circle4 = {
+    x: 500,
+    y: 165, 
+    size: 68,
+    speed: 2, 
+    fill: 100
 };
 let circle2 = {
-    x: 500,
+    x: 0,
     y: 250, 
-    size: 75,
-    speed: 2, 
+    size: 100,
+    speed: 2,
     fill: 255
+};
+let circle5 = {
+    x: 0,
+    y: 367, 
+    size: 115,
+    speed: 2, 
+    fill: 100
 };
 let circle3 = {
-    x: 250,
-    y: 0, 
-    size: 100,
+    x: 500,
+    y: 462, 
+    size: 75,
     speed: 2, 
-    fill: 255
+    fill: 150
 };
+let circle6 = {
+    x: 250,
+    y: 254, 
+    size: 110,
+    speed: 2,
+    fill: 255
+}
 
 /**
- * Description of preload
+ * Does nothing.
 */
 function preload() {
 
 }
 
-
 /**
- * Description of setup
+ * Creates the canvas and ensures the shapes have no outline.
 */
 function setup() {
     createCanvas(500, 500);
     noStroke();
 }
 
-
 /**
- * Description of draw()
+ * Draws the 6 circles and displays the various size, colour, movement changes.
 */
 function draw() {
     // Background 
     background(bg.r, bg.g, bg.b);
-    bg.r = bg.r + 1;
+    bg.r = map(circle2.size, 100, width, 0, 255); 
+    
+    // Circle 2 (first in the list as its the backmost circle)
+    circle2.x = circle2.x + -1;
+    circle2.x = constrain(circle2.x, width/2, width);
+    circle2.size = circle2.size + 0.75;
+    circle2.size = constrain(circle2.size, 0, width);
+    fill(circle2.fill);
+    ellipse(circle2.x, circle2.y, circle2.size);
 
-    // White square that moves according to mouse movements
-    //rectMode(CENTER);
-    //rect(mouseX, mouseY, 100, 100);
+    // Circle 6 (circle that responds to mouseX and mouseY commands)
+    circle6.fill = map(mouseX, 0, width, 0, 255);
+    fill(circle6.fill);
+    ellipse(circle6.x, circle6.y, circle6.size);
 
-    // Circle 1
+    // Circle 1 (the frontmost circle)
     circle1.x = circle1.x + 1;
     circle1.x = constrain(circle1.x, 0, width/2);
     fill(circle1.fill);
     ellipse(circle1.x, circle1.y, circle1.size);
 
-    // Circle 2
-    circle2.x = circle2.x + -1;
-    circle2.x = constrain(circle2.x, width/2, width);
-    fill(circle2.fill);
-    ellipse(circle2.x, circle2.y, circle2.size);
+    // Circle 4 (the frontmost circle)
+    circle4.x = circle4.x + -1;
+    circle4.x = constrain(circle4.x, width/2, width);
+    //circle4.size = circle4.size + 0.75;
+    //circle4.size = constrain(circle4.size, 0, width);
+    fill(circle4.fill);
+    ellipse(circle4.x, circle4.y, circle4.size);
 
-    // Circle 3
+    // Circle 5 (the frontmost circle)
+    circle5.x = circle5.x + 1;
+    circle5.x = constrain(circle5.x, 0, width/2);
+    fill(circle5.fill);
+    ellipse(circle5.x, circle5.y, circle5.size);
+
+    // Circle 3 (the frontmost circle)
+    circle3.x = circle3.x + -1;
+    circle3.x = constrain(circle3.x, width/2, width);
     fill(circle3.fill);
     ellipse(circle3.x, circle3.y, circle3.size);
 }
