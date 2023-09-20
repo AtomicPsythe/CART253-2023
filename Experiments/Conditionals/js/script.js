999324c3547a580fd859e11d77fe41993777ef6e
Foti Aivaliklis <fotibuddy@gmail.com>
2023-09-19 20:35:23 -0400

    Renamed a folder + started working on conditionals

---
+++ Experiments/Conditionals/js/script.js
@@ -0,0 +1,48 @@
+/**
+ * Conditionals
+ * Foti Aivaliklis
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+let backgroundShade = 0;
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100, 
+    speed: 1
+}
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+    createCanvas(500, 500);
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+    background(backgroundShade);
+
+    circle.x = circle.x + circle.speed;
+
+    if (circle.x > width) {
+        circle.speed = -circle.speed;
+    }
+
+    ellipse(circle.x, circle.y, circle.size);
+}
