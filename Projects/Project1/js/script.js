2e9865e6f7a60b5a7600e39506954d56cbddb8db
AtomicPsythe <fotibuddy@gmail.com>
2023-10-05 19:10:24 -0400

    Added one object, the background, made the object moveable and started working on the title screen

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -19,11 +19,20 @@ let shape = {
     active: true
 };
 
+// image variables
+let knife;
+let kitchen;
+
+// various states
+let state = "title";
+let instructions = "Click and drag the knife to different ingredients to prep them and combine them with others to create a beautiful dish!";
+
 /**
  * Description of preload
 */
 function preload() {
-
+    knife = loadImage("assets/images/knife_edited.png");
+    kitchen = loadImage("assets/images/kitchen.jpg");
 }
 
 
@@ -31,8 +40,8 @@ function preload() {
  * Description of setup
 */
 function setup() {
-    createCanvas(700, 300);
-    shape.x = width/4;
+    createCanvas(1000, 800);
+    shape.x = width/2;
     shape.y = height/2;
 }
 
@@ -41,36 +50,70 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(0);
+    background(kitchen);
+
+    if (state === "title") {
+        title();
+    }
+    else if (state === "simulation") {
+        simulation();
+    }
 
     if (shape.active) {
         handleDragging();
         drawShape();
     }
-    drawBoundary();
 }
 
-function handleDragging() {
-    if (shape.isBeingDragged) {
-        shape.x = mouse.x + shape.offSetX;
-        shape.y = mouse.y + shape.offSetY;
-
-        shape.x = constrain(shape.x, 0, width);
-        shape.y = constrain(shape.y, 0, height);
-    }
+// the title page of the simulation before it begins
+function title() {
+    background(150)
+    push();
+    noFill();
+    strokeWeight(3);
+    rect(320, 170, 400, 60, 20);
+    textSize(40);
+    fill(159, 51, 51);
+    textAlign(CENTER, CENTER);
+    text("Cooking Simulator", 515, 200);
+    pop();
+    push();
+    noFill();
+    rect(320, 300, 400, 200, 20);
+    textSize(20);
+    fill(159, 51, 51);
+    textAlign(CENTER);
+    textStyle(BOLD);
+    text("Rules", 328, 305, 400, 200);
+    pop();
+    textSize(20);
+    fill(159, 51, 51);
+    textAlign(CENTER);
+    text(instructions, 320, 335, 400, 200);
 }
 
 function drawShape() {
     push();
     fill(255, 0, 0);
     noStroke();
-    ellipse(shape.x, shape.y, shape.size);
+    image(knife, shape.x, shape.y, shape.size);
     pop();
+}
+
+// Interacting with the objects functions
+function handleDragging() {
+    if (shape.isBeingDragged) {
+        shape.x = mouseX + shape.offSetX;
+        shape.y = mouseY + shape.offSetY;
+
+        shape.x = constrain(shape.x, 0, width);
+        shape.y = constrain(shape.y, 0, height);
+    }
 }
 
 function mouseIsInsideShape() {
     let d = dist(mouseX, mouseY, shape.x, shape.y);
-    if (d < shape.size/2) {
+    if (d < shape.size) {
         return true;
     }
     else {
@@ -78,28 +121,21 @@ function mouseIsInsideShape() {
     }
 }
 
-function drawBoundary() {
-    push();
-    stroke(255);
-    line(width/2, 0, width/2, height);
-    pop();
-}
-
+// Mouse related functions
 function mousePressed() {
+    if (state === "title") {
+        state === "simulation";
+    }
+
     if (shape.active && mouseIsInsideShape()) {
         shape.isBeingDragged = true;
-        shape.offSetX = shape.x - mouse.x;
-        shape.offSetY = shape.y - mouse.y;
-        shape.size -= shape.feedbackSizeChangeAmount;
+        shape.offSetX = shape.x - mouseX;
+        shape.offSetY = shape.y - mouseY;
     }
 }
 
 function mouseReleased() {
-    if (shape.isBeingDragged && shape.x > width/2) {
-        shape.active = false;
-    }
-    else {
-        shape.isBeingDragged = false;
+    if (shape.isBeingDragged = false) {
         shape.size += shape.feedbackSizeChangeAmount;
         shape.offSetX = 0;
         shape.offSetY = 0;
