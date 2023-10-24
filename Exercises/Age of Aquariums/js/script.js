c8939785c617d7b73de536c4bdb28b11cb26e310
AtomicPsythe <fotibuddy@gmail.com>
2023-10-24 18:54:27 -0400

    Finally completed the Age of Aquariums exercise!

--- Exercises/Age of Aquariums/js/script.js
+++ Exercises/Age of Aquariums/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Exercise 4: Age of Aquariums
- * Foti Aivalikli
+ * Foti Aivaliklis
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -28,8 +28,8 @@ let state = "title" // can be title, simulation, ending1, ending2
  * Description of preload
 */
 function preload() {
-    fish = loadImage("assets/images/fish.png");
     shark = loadImage("assets/images/shark.png");
+    ocean = loadImage("assets/images/ocean2.jpg");
 }
 
 
@@ -82,22 +82,38 @@ function draw() {
 }
 
 function title() {
+    background(ocean);
     push();
-    textSize(40);
-    fill(0, 250, 250);
+    fill(255);
+    stroke(0, 0, 139);
+    strokeWeight(8);
+    rect(80, 165, 440, 70);
+    pop();
+    push();
+    textSize(45);
+    fill(179, 98, 0);
     textAlign(CENTER, CENTER);
     text("Age of Aquariums", width/2, height/3);
     pop();
     push();
+    fill(255);
+    stroke(0, 0, 139);
+    strokeWeight(5);
+    rect(30, 280, 540, 110);
+    pop();
+    push();
     textSize(20);
-    fill(250, 250, 250);
+    fill(179, 98, 0);
     textAlign(CENTER, CENTER);
     text("Click the screen to begin the simulation!", width/2, height/2);
+    textSize(15);
+    text("As the shark, your goal is to avoid the fish circles, \n while using the keyboard keys to spawn more to not disrupt \n the relationship between both groups. If you do... special things will happen...!", width/2, height/1.7);
+    pop();
 }
 
 function simulation() {
     push();
-
+    background(ocean);
     pop();
     mouseImage();
     for (let i = 0; i < school.length; i++) {
@@ -109,20 +125,34 @@ function simulation() {
 }
 
 function ending1() {
+    background(ocean);
+    push();
+    fill(255);
+    stroke(0, 0, 139);
+    strokeWeight(5);
+    rect(40, 265, 520, 70);
+    pop();
     push();
     textSize(20);
-    fill(255, 255, 255);
+    fill(179, 98, 0);
     textAlign(CENTER, CENTER);
-    text("The peace between the fish and the sharks has ended, \n this means war!", width/2, height/2);
+    text("The peace between the fish and the sharks has ended, \n this means war! AAAAAAAAAAAAAA", width/2, height/2);
     pop();
 }
 
 function ending2() {
+    background(ocean);
+    push();
+    fill(255);
+    stroke(0, 0, 139);
+    strokeWeight(5);
+    rect(40, 265, 520, 70);
+    pop();
     push();
     textSize(20);
-    fill(255, 255, 255);
+    fill(179, 98, 0);
     textAlign(CENTER, CENTER);
-    text("Woah, a rare species of fish has spawned!", width/2, height/2);
+    text("Woah, a rare species of fish has spawned! \n Let's let it roam free to not make it go extinct.", width/2, height/2);
     pop();
 }
 
@@ -161,11 +191,6 @@ function displayFish(fish) {
     fill(fish.color);
     ellipse(fish.x, fish.y, fish.size);
     pop();
-    // push();
-    // fill(200, 100, 100);
-    // noStroke();
-    // ellipse(fish.x, fish.y, fish.size);
-    // pop();
 }
 
 function specialColor(fish) {
