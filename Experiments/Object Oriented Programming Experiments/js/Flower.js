976536006eef27a186985dec367977924f596d5c
AtomicPsythe <fotibuddy@gmail.com>
2023-10-25 18:31:51 -0400

    Completed all of the OOP lecture videos just need to reconfirm if the code works as it is not right now

--- Experiments/Object Oriented Programming Experiments/js/Flower.js
+++ Experiments/Object Oriented Programming Experiments/js/Flower.js
@@ -1,27 +1,45 @@
 class Flower {
 
     constructor(x, y, size, stemLength, petalColor) {
-        this.x = random(0, width);
-        this.y = random(0, height);
-        this.size = 50;
-        this.stemLength = 75; 
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.maxSize = size;
+        this.stemLength = stemLength; 
         this.stemThickness = 10;
         this.petalThickness = 10;
+        this.maxPetalThickness = 10;
         this.stemColor = {
             r: 50,
             g: 150,
             b: 50
         };
-        this.petalColor = {
-            r: 200, 
-            g: 50,
-            b: 50
-        };
+        this.petalColor = petalColor;
         this.centreColor = {
             r: 50,
             g: 0, 
             b: 0
+        };
+        this.alive = true;
+    }
+
+    shrink() {
+        let shrinkage = random(0, 0.1);
+        this.size = this.size - shrinkage;
+        this.petalThickness = this.petalThickness - shrinkage/10;
+
+        if (this.size <= 0 || this.petalThickness <= 0) {
+            this.alive = false;
         }
+    }
+
+    pollinate() {
+        let growth = random(0, 0.5);
+        this.size = this.size + growth;
+        this.petalThickness = this.petalThickness + growth/10;
+
+        this.size = constrain(this.size, 0, this.maxSize);
+        this.petalThickness = constrain(this.petalThickness, 0, this.maxPetalThickness);
     }
 
     display() {
@@ -34,6 +52,14 @@ class Flower {
         stroke(this.petalColor.r, this.petalColor.g, this.petalColor.b);
         ellipse(this.x, this.y, this.size);
         pop();
+    }
+
+    mousePressed() {
+        let d = dist(this.x, this.y, mouseX, mouseY);
+        if (d < this.size/2 + this.petalThickness) {
+            this.stemLength = this.stemLength + 5;
+            this.y = this.y - 5;
+        }
     }
 
 }
--- Experiments/Object Oriented Programming Experiments/js/script.js
+++ Experiments/Object Oriented Programming Experiments/js/script.js
@@ -8,8 +8,10 @@
 let garden = {
     // array that creates the flowers
     flowers: [],
+    bees: [],
     // how many flowers are made
-    numFlowers: 100, 
+    numFlowers: 30, 
+    numBees: 7,
     // creates the grass
     grassColor: {
         r: 120,
@@ -36,9 +38,14 @@ function setup() {
             b: random(100, 255)
         };
         // creates new flower
-        let flower = new Flower();
+        let flower = new Flower(x, y, size, stemLength, petalColor);
         // adds flowers to the array
         garden.flowers.push(flower);
+    }
+
+    for (let i = 0; i < garden.numBees; i++) {
+        let bee = new Bee(random(0, width), random(0, height));
+        garden.bees.push(bee);
     }
 }
 
@@ -50,6 +57,32 @@ function draw() {
 
     for (let i = 0; i < garden.flowers.length; i++) {
         let flower = garden.flowers[i];
-        flower.display();
+        if (flower.alive) {
+            flower.shrink();
+            flower.display();
+        }
+    }
+
+    for (let i = 0; i < garden.bees.length; i++) {
+        let bee = garden.bees[i];
+        if (bee.alive) {
+            bee.shrink();
+            bee.move();
+            bee.display();
+
+            for (let j = 0; j < garden.flowers.length; j++) {
+                let flower = garden.flowers[j];
+                if (flower.alive) {
+                    bee.tryToPollinate(flower);
+                }
+            }
+        }
+    }
+}
+
+function mousePressed() {
+    for (let i = 0; i < garden.flowers.length; i++) {
+        let flower = garden.flowers[i];
+        flower.mousePressed();
     }
 }
