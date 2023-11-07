14d3f63d96dc465f854f3fe2c3a75000cd8a94e9
AtomicPsythe <fotibuddy@gmail.com>
2023-11-07 16:40:04 -0500

    Started the sound experiments lecture videos

---
+++ Experiments/Sound Experiments/js/script.js
@@ -0,0 +1,29 @@
+/**
+ * Sound Experiments
+ * Foti Aivaliklis
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
